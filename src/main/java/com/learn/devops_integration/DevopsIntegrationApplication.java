4416afa2be2fb07c244bd9f3de92dabae39d2ca9
harshani24 <harshaninimanthiperera@gmail.com>
2025-09-27 13:04:02 +0530

    introduce get endpoint which send message

--- src/main/java/com/learn/devops_integration/DevopsIntegrationApplication.java
+++ src/main/java/com/learn/devops_integration/DevopsIntegrationApplication.java
@@ -2,9 +2,17 @@ package com.learn.devops_integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsIntegrationApplication {
+
+	@GetMapping
+	public String message(){
+		return "Welcome to DevOps Integration Learning";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
